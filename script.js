b40197ad10749378150a69d67ec182ff3efdf0df
loboplateado <loboplateado1203@gmail.com>
2024-08-13 13:19:03 -0300

    cambios en la funcion encriptar
    se visibilizo el  boton copiar .
    e cambio el color de fondo del boton encriptar

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const textArea= document.querySelector(".textarea");// se almacenaloq el usuarioescribe
 const textMensaje= document.querySelector(".textMensaje");
 const botonCopia= document.querySelector(".boton_copia");
+const botonDesen= document.querySelector(".botonDesencriptar");
 
 //La letra "e" es convertida para "enter"
 //La letra "i" es convertida para "imes"
@@ -8,23 +9,7 @@ const botonCopia= document.querySelector(".boton_copia");
 //La letra "o" es convertida para "ober"
 //La letra "u" es convertida para "ufat"
 
-//@@@@@@@@@@@@@@@@@@@@@@@--Encriptar--@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
-function encriptar(stringEncriptado){
-    
-    let matrizCodigo = [["a","ai"],["e","enter"],["i","imes"],["o","ober"],["u","ufat"]];//carga de matriz
-    stringEncriptado = stringEncriptado.toLowerCase();//convercion a minuscula
-        
-        for(let i = 0; i< matrizCodigo.length ;i++){//Recorre la matriz
-
-            if (stringEncriptado.includes (matrizCodigo[i][0])){//verificacion de las letrasque se ingresan 
-                stringEncriptado= stringEncriptado.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1]);//replaceAllcambia una cadena de caracteres por otros
-            }
-        }
-
-    return stringEncriptado;
-}
-
+//QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
 function btnEncriptar(){
     
     const textoEncriptado = encriptar(textArea.value);//recibe la funcion encriptar,recibe la const textArea
@@ -33,41 +18,19 @@ function btnEncriptar(){
     textArea.value="Ingrese el texto aqui";
     textMensaje.style.backgroundImage ="none";//oculta la imagen cuando aparece el texto encriptado
     var tex = document.getElementById("mensajeCopi");// oculta el texto cuando aparece el textEncriptado
-        
-        if (tex.style.visibility === "collapse") {//
-            tex.style.visibility = "visible";
-        } else {
-            tex.style.visibility = "collapse";//
-            
-        }
-     
+    tex.style.visibility = "collapse";// hace invisible el texto dentro de div mensaje_copi
+    botonCopia.style.visibility = "visible";// hace visible el boton copiar
+    botonDesen.style.background = "#E5E5E5";// cambia el color del boton
 }
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@--Desencriptar--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-function desencriptar(stringDesencriptado){
+function encriptar(stringEncriptado){
     
     let matrizCodigo = [["a","ai"],["e","enter"],["i","imes"],["o","ober"],["u","ufat"]];//carga de matriz
-    stringDesencriptado = stringDesencriptado.toLowerCase();//convercion a minuscula
+    stringEncriptado = stringEncriptado.toLowerCase();//convercion a minuscula 
         for(let i = 0; i< matrizCodigo.length ;i++){//Recorre la matriz
-            if (stringDesencriptado.includes(matrizCodigo[i][1])){//verificacion de las letrasque se ingresan  
-                stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0]);
+            if (stringEncriptado.includes (matrizCodigo[i][0])){//verificacion de las letrasque se ingresan 
+                stringEncriptado= stringEncriptado.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1]);//replaceAllcambia una cadena de caracteres por otros
             }
         }
-    return stringDesencriptado;
+    return stringEncriptado;
 }
-
-function btnDesencriptar(){
-    
-    const textoEncriptado= desencriptar(textArea.value);
-    textMensaje.value =textoEncriptado; 
-    textArea.value="";
-  /*  var copi = document.getElementById("btnCopi");
-        
-        if (copi.style.visibility === "hidden ") {//collapse
-            copi.style.visibility = "visible";
-        } else {
-            copi.style.visibility = " hidden";//collapse
-        }
-    ocultar();*/
-}
-
