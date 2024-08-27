aab546b04ff449d1901d7d40c98335a336d9e4a3
loboplateado <loboplateado1203@gmail.com>
2024-08-27 14:27:29 -0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -25,7 +25,7 @@ function btnEncriptar(){
 }
 function encriptar(stringEncriptado){
     
-    let matrizCodigo = [["a","ai"],["e","enter"],["i","imes"],["o","ober"],["u","ufat"]];//carga de matriz
+    let matrizCodigo = [["e","enter"],["i","imes"],["o","ober"],["a","ai"],["u","ufat"]];//carga de matriz
     stringEncriptado = stringEncriptado.toLowerCase();//convercion a minuscula 
         for(let i = 0; i< matrizCodigo.length ;i++){//Recorre la matriz
             if (stringEncriptado.includes (matrizCodigo[i][0])){//verificacion de las letrasque se ingresan 
@@ -45,7 +45,7 @@ function btnDesencriptar(){
 
 function desencriptar(stringDesencriptado){
     
-    let matrizCodigo = [["a","ai"],["e","enter"],["i","imes"],["o","ober"],["u","ufat"]];//carga de matriz
+    let matrizCodigo = [["e","enter"],["i","imes"],["o","ober"],["a","ai"],["u","ufat"]];//carga de matriz
     stringDesencriptado = stringDesencriptado.toLowerCase();//convercion a minuscula
         for(let i = 0; i< matrizCodigo.length ;i++){//Recorre la matriz
             if (stringDesencriptado.includes(matrizCodigo[i][1])){//verificacion de las letrasque se ingresan  
